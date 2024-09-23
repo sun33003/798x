267d823c4713014b78922c2c677700bb9248b0c3
sun33003 <127114580+sun33003@users.noreply.github.com>
2024-09-23 17:35:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git watchcat https://github.com/ATang007ZH/mywatchcat' >>feeds.conf.default
 #echo 'src-git watchcat https://github.com/openwrt/packages/tree/master/utils' >>feeds.conf.default
